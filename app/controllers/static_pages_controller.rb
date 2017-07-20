5dfe33ef2c8f2bcd9bd61a5117e17d5a0b7a02be
nathalie <nathalie19@live.co.za>
2017-07-21 10:17:31 +1200

    4.5 added a carousel and partial in landing page,added code in partial product html, added additional indicators

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,8 +2,12 @@ class StaticPagesController < ApplicationController
   def index
   end
 
-  def landing_page
-  	@featured_product = Product.first
-  end
+  #def landing_page
+  	#@featured_product = Product.first
+  #end
 
+  def landing_page
+  	@products = Product.limit(6)
+  end
+  
 end
