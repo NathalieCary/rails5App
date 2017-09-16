7ea55fde6b45eaf60349dd0771421254d8f4c08f
nathalie <nathalie19@live.co.za>
2017-09-16 15:30:57 +1200

    6.7 byebugs

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,13 +4,14 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
+    #byebug
     if params[:q]
       search_term = params[:q]
       @products = Product.search(search_term)
-      logger.debug "Product: #{@products}"
+      #logger.debug "Product: #{@products}"
     else
       @products = Product.all
-      logger.debug "Product: #{@products}"
+      #logger.debug "Product: #{@products}"
     end
 
       @products = @products.paginate(:page => params[:page], :per_page => 9 )
