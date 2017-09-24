e5b62d3fb9e3c206ed49b501ea3c10f950151ffe
nathalie <nathalie19@live.co.za>
2017-09-24 13:32:55 +1300

    6.9 Jquery elevateZoom

--- app/assets/javascripts/fade.js
+++ app/assets/javascripts/fade.js
@@ -1,4 +1,4 @@
-$(document).on('turbolinks:load', function(){
+ $(document).on('turbolinks:load', function(){
   $('.alert').delay(500).fadeIn('normal', function () {
 		$(this).delay(2500).fadeOut ();
 	});  
