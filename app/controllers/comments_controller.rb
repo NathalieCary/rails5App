3f2ee09d879eda6b8f3fee031c6c5f0a39e69d92
nathalie <nathalie19@live.co.za>
2017-10-05 12:22:41 +1300

    Added ActiveJob

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,29 +1,30 @@
 class CommentsController < ApplicationController
+	
 	def create
 		@product = Product.find(params[:product_id])
 		@comment = @product.comments.new(comment_params)
 		@comment.user = current_user
 		@user = current_user
-    respond_to do |format|
-      if @comment.save
-        ActionCable.server.broadcast 'product_channel', comment: @comment, average_rating:
-        @comment.product.average_rating    
-        format.html { redirect_to @product, notice: 'Review was created successfully.' }      
-        format.json { render :show, status: :created, location: @product }
-        format.js
-      else        
-        format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
+		    respond_to do |format|
+		      if @comment.save
+		        format.html { redirect_to @product, notice: 'Review was created successfully.' }      
+		        format.json { render :show, status: :created, location: @product }
+		        format.js
+		      else        
+		        format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
+		        format.json { render json: @comment.errors, status: :unprocessable_entity }
+		      end
+    	end
 	end
+
 
 	def destroy
 		@comment = Comment.find(params[:id])
-	  product = @comment.product
-	  @comment.destroy
-	  redirect_to product
+		  product = @comment.product
+		  @comment.destroy
+		  redirect_to product
 	end
+	
 
 	private
 	# Using a private method to encapsulate the permissible parameters is
